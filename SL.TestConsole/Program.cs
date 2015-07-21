fd5a7d520f2a0e7ccc338225668b33a06ddc2e40
Felipe <felipegte@gmail.com>
2015-07-21 11:12:53 -0300

    Load up

---
+++ SL.TestConsole/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace SL.TestConsole
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			/// Given any object instance
+			var list = new List<int>();
+
+			/// We create the simple locker instance
+			var locker = new SimpleLocker();
+
+			Parallel.For(0, 10000, i=>
+				{
+					/// Safelly modify it
+					locker.WriteVoid(() => list.Add(i));
+
+					/// Safelly ready it
+					Console.WriteLine("CurrentCount {0} - ThreadId: {1}", locker.Read(() => list.Count), Thread.CurrentThread.ManagedThreadId);
+
+				});
+
+			Console.WriteLine ("Done!");
+			Console.ReadLine ();
+		}
+	}
+}
---
+++ SL/SimpleLocker.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Threading;
+
+namespace SL
+{
+	public sealed class SimpleLocker
+	{
+		private readonly ReaderWriterLockSlim _locker = new ReaderWriterLockSlim();
+		private int _lockTimeout = 1000;
+
+		public SimpleLocker(int lockTimeout = 1000)
+		{
+			_lockTimeout = lockTimeout;
+		}
+
+		public ReturnType Read<ReturnType>(Func<ReturnType> what)
+		{
+			if (!_locker.TryEnterReadLock(_lockTimeout)) { throw new TimeoutException(); }
+			try
+			{
+				var result = what();
+				_locker.ExitReadLock();
+				return result;
+			}
+			catch
+			{
+				if (_locker.IsReadLockHeld)
+				{
+					_locker.ExitReadLock();
+				}
+				throw;
+			}
+		}
+
+		public ReturnType UpgradeableRead<ReturnType>(Func<ReturnType> what)
+		{
+			if (!_locker.TryEnterUpgradeableReadLock(_lockTimeout)) { throw new TimeoutException(); }
+			try
+			{
+				var result = what();
+				_locker.ExitUpgradeableReadLock();
+				return result;
+			}
+			catch
+			{
+				if (_locker.IsUpgradeableReadLockHeld)
+				{
+					_locker.ExitUpgradeableReadLock();
+				}
+				throw;
+			}
+		}
+
+		public ReturnType Write<ReturnType>(Func<ReturnType> what)
+		{
+			if (!_locker.TryEnterWriteLock(_lockTimeout)) { throw new TimeoutException(); }
+			try
+			{
+				var result = what();
+				_locker.ExitWriteLock();
+				return result;
+			}
+			catch
+			{
+				if (_locker.IsWriteLockHeld)
+				{
+					_locker.ExitWriteLock();
+				}
+				throw;
+			}
+		}
+
+		public void ReadVoid(Action what)
+		{
+			if (!_locker.TryEnterReadLock(_lockTimeout)) { throw new TimeoutException(); }
+			try
+			{
+				what();
+				_locker.ExitReadLock();
+			}
+			catch
+			{
+				if (_locker.IsReadLockHeld)
+				{
+					_locker.ExitReadLock();
+				}
+				throw;
+			}
+		}
+
+		public void UpgradeableReadVoid(Action what)
+		{
+			if (!_locker.TryEnterUpgradeableReadLock(_lockTimeout)) { throw new TimeoutException(); }
+			try
+			{
+				what();
+				_locker.ExitUpgradeableReadLock();
+			}
+			catch
+			{
+				if (_locker.IsUpgradeableReadLockHeld)
+				{
+					_locker.ExitUpgradeableReadLock();
+				}
+				throw;
+			}
+		}
+
+		public void WriteVoid(Action what)
+		{
+			if (!_locker.TryEnterWriteLock(_lockTimeout)) { throw new TimeoutException(); }
+			try
+			{
+				what();
+				_locker.ExitWriteLock();
+			}
+			catch
+			{
+				if (_locker.IsWriteLockHeld)
+				{
+					_locker.ExitWriteLock();
+				}
+				throw;
+			}
+		}
+	}
+
+}
